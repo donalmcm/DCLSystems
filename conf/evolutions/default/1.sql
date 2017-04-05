c75023c60cc00b5d62957b94c15b4cb5282911d3
Enda Lee <enda.lee@ittdublin.ie>
2017-04-05 12:03:38 +0100

    Login

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -29,6 +29,14 @@ create table tank (
 );
 create sequence tank_seq;
 
+create table user (
+  email                         varchar(255) not null,
+  role                          varchar(255),
+  name                          varchar(255),
+  password                      varchar(255),
+  constraint pk_user primary key (email)
+);
+
 
 # --- !Downs
 
@@ -37,4 +45,6 @@ drop sequence if exists fuel_sales_seq;
 
 drop table if exists tank;
 drop sequence if exists tank_seq;
+
+drop table if exists user;
 
---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,3 @@
+insert into user (email,name,password,role) values ( 'Donal@gmail.com', 'Donal McManus', 'password', 'admin' );
+insert into user (email,name,password,role) values ( 'Ciara@gmail.com', 'Ciara Walsh', 'password', 'admin' );
+insert into user (email,name,password,role) values ( 'Leon@gmail.com', 'Leon Walker OBrien', 'password', 'admin' );
