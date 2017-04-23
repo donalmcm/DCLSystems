1f8cfea92c1ea25152098d7200e27af8a4edb7d5
Enda Lee <enda.lee@ittdublin.ie>
2017-04-24 00:13:50 +0100

    js inserted for graphs

--- public/js/sb-admin-2.js
+++ public/js/sb-admin-2.js
@@ -66,3 +66,78 @@ function drawChart() {
 
     chart.draw(data, options);
 }
+
+function drawVisualization() {
+    // Create and populate the data table.
+    var data = google.visualization.arrayToDataTable([
+        ['Month', 'Unleaded', 'Diesel'],
+        ['January',  9588,    24480],
+        ['February',  11276,    26788],
+        ['March',  13137,    26939],
+        ['April',  9986,    24513],
+        ['May',  12908,    27523],
+        ['June',  11778,    26438],
+        ['July',  11339,    26886],
+        ['August',  13802,    27847],
+        ['September',  10743,    25977],
+        ['October',  13898,    26645],
+        ['November',  10082,    25199],
+        ['December',  12973,    25793],
+    ]);
+
+    // Create and draw the visualization.
+    new google.visualization.ColumnChart(document.getElementById('barchart')).
+    draw(data,
+        {title:"Diesel vs Unleaded Sales",
+            width:900, height:500,
+            vAxis: {title: "Sales"}, isStacked: true,
+            hAxis: {title: "Month"}}
+    );
+}
+
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawVisualization);
+
+
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawCurveTypes);
+
+function drawCurveTypes() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Unleaded');
+    data.addColumn('number', 'Diesel');
+
+    data.addRows([
+        [0, 0, 0],    [1, 10, 5],   [2, 23, 15],  [3, 17, 9],   [4, 18, 10],  [5, 9, 5],
+        [6, 11, 3],   [7, 27, 19],  [8, 40, 40],  [9, 40, 32],  [10, 32, 24], [11, 35, 27],
+        [12, 30, 22], [13, 40, 32], [14, 42, 34], [15, 47, 39], [16, 44, 36], [17, 48, 40],
+        [18, 52, 44], [19, 54, 46], [20, 42, 34], [21, 55, 47], [22, 56, 48], [23, 57, 49],
+        [24, 60, 52], [25, 50, 42], [26, 52, 44], [27, 51, 43], [28, 49, 41], [29, 53, 45],
+        [30, 55, 47], [31, 60, 52], [32, 61, 53], [33, 59, 51], [34, 62, 54], [35, 65, 57],
+        [36, 62, 54], [37, 58, 50], [38, 55, 47], [39, 61, 53], [40, 64, 56], [41, 65, 57],
+        [42, 63, 55], [43, 66, 58], [44, 67, 59], [45, 69, 61], [46, 69, 61], [47, 70, 62],
+        [48, 72, 64], [49, 68, 60], [50, 66, 58], [51, 65, 57], [52, 67, 59], [53, 70, 62],
+        [54, 71, 63], [55, 72, 64], [56, 73, 65], [57, 75, 67], [58, 70, 62], [59, 68, 60],
+        [60, 64, 56], [61, 60, 52], [62, 65, 57], [63, 67, 59], [64, 68, 60], [65, 69, 61],
+        [66, 70, 62], [67, 72, 64], [68, 75, 67], [69, 80, 72]
+    ]);
+
+    var options = {
+        hAxis: {
+            title: 'Time'
+        },
+        vAxis: {
+            title: 'Popularity'
+        },
+        series: {
+            1: {curveType: 'function'}
+        }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
+
+
+
